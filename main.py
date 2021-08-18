b8c3a332f43b7ff115c9871624644b350c6aa445
sergey <Proshchenko@innoscripta.com>
2021-08-18 21:31:07 +0300

    hello world

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('hello world')
+from problems.concurrency.problem_1114_print_in_order import task
+
+
+class RunProblems:
+
+    @staticmethod
+    def run_task():
+        task()
+
+
+if __name__ == '__main__':
+    run_problems = RunProblems()
+    run_problems.run_task()
