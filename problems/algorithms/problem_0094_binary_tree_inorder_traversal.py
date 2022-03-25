e122d3f7a98b9f43f132279286939aafba24e272
Sergey <sproshchenko@yandex.ru>
2022-03-25 21:26:05 +0300

    - solved problem_0094_binary_tree_inorder_traversal.py

---
+++ problems/algorithms/problem_0094_binary_tree_inorder_traversal.py
@@ -0,0 +1,40 @@
+from typing import Optional, List
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    # iterative solution
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        stack = []
+        cur = root
+
+        while cur or stack:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+            cur = stack.pop()
+            res.append(cur)
+            cur = cur.right
+        return res
+
+    # recursive solution
+    def inorderTraversal2(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+
+        def inorder(root):
+            if not root:
+                return
+            inorder(root.left)
+            res.append(root.val)
+            inorder(root.right)
+
+        inorder(root)
+        return res
