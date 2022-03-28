39cdb9c4522b7f3cb73033a9a7df1171b4157164
Sergey <sproshchenko@yandex.ru>
2022-03-28 20:55:37 +0300

    - solved problem_0024_swap_nodes_in_pairs.py

---
+++ problems/algorithms/problem_0024_swap_nodes_in_pairs.py
@@ -0,0 +1,26 @@
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        prev, cur = dummy, head
+
+        while cur and cur.next:
+            nextPair = cur.next.next
+            second = cur.next
+
+            second.next = cur
+            cur.next = nextPair
+            prev.next = second
+
+            prev = cur
+            cur = nextPair
+        return dummy.next
