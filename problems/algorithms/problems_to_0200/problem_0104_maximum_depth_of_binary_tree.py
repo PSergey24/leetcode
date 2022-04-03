9299b8eba3de0081d9a5a0c23b6915ea1a71eaf3
Sergey <sproshchenko@yandex.ru>
2022-04-03 22:10:42 +0300

    - solved problem_0104_maximum_depth_of_binary_tree.py

---
+++ problems/algorithms/problems_to_0200/problem_0104_maximum_depth_of_binary_tree.py
@@ -0,0 +1,50 @@
+from collections import deque
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    # iterative DFS
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        stack = [[root, 1]]
+        res = 0
+
+        while stack:
+            node, depth = stack.pop()
+            if node:
+                res = max(res, depth)
+                stack.append([node.left, depth + 1])
+                stack.append([node.right, depth + 1])
+        return res
+
+    # BFS
+    def maxDepth_BFS(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        level = 0
+        q = deque([root])
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            level += 1
+        return level
+
+    # recursive DFS
+    def maxDepth_DFS(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        return 1 + max(self.maxDepth_DFS(root.left), self.maxDepth_DFS(root.right))
+
+
