073bf555e170c09a970fd7d50e0bdec894cd5dc0
Sergey <sproshchenko@yandex.ru>
2022-04-13 15:12:36 +0300

    - problem solved problem_0059_spiral_matrix_II.py

---
+++ problems/algorithms/problems_to_0100/problem_0059_spiral_matrix_II.py
@@ -0,0 +1,31 @@
+from typing import List
+
+
+class Solution:
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        matrix = [[0] * n for i in range(n)]
+        top, bottom, left, right = 0, n - 1, 0, n - 1
+
+        val = 1
+        while val <= n ** 2:
+            for i in range(left, right + 1, 1):
+                matrix[top][i] = val
+                val += 1
+            top += 1
+
+            for i in range(top, bottom + 1, 1):
+                matrix[i][right] = val
+                val += 1
+            right -= 1
+
+            for i in range(right, left - 1, -1):
+                matrix[bottom][i] = val
+                val += 1
+            bottom -= 1
+
+            for i in range(bottom, top - 1, -1):
+                matrix[i][left] = val
+                val += 1
+            left += 1
+        return matrix
+
