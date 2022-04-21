7f3e25b5c42cd973c8a4e6119853d8022fb81581
Sergey <sproshchenko@yandex.ru>
2022-04-21 15:17:38 +0300

    - problem solved problem_0705_design_hashSet.py

---
+++ problems/algorithms/problem_0705_design_hashSet.py
@@ -0,0 +1,31 @@
+class MyHashSet:
+
+    def __init__(self):
+        self.size = 10000
+        self.table = [None] * self.size
+
+    def calculate_hash_value(self, key):
+        return key % self.size
+
+    def add(self, key: int) -> None:
+        hv = self.calculate_hash_value(key)
+
+        if self.table[hv] is None:
+            self.table[hv] = [key]
+        else:
+            self.table[hv].append(key)
+
+    def remove(self, key: int) -> None:
+        hv = self.calculate_hash_value(key)
+
+        if self.table[hv] is not None:
+            while key in self.table[hv]:
+                self.table[hv].remove(key)
+
+    def contains(self, key: int) -> bool:
+        hv = self.calculate_hash_value(key)
+
+        if self.table[hv] is not None:
+            return key in self.table[hv]
+        return False
+
