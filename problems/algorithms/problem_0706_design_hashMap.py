0c62a1804017b8875b682e5f30b1616a22ba549b
Sergey <sproshchenko@yandex.ru>
2022-04-22 15:21:45 +0300

    - problem solved problem_0706_design_hashMap.py

---
+++ problems/algorithms/problem_0706_design_hashMap.py
@@ -0,0 +1,13 @@
+class MyHashMap:
+
+    def __init__(self):
+        self.l = [-1] * 1000001
+
+    def put(self, key: int, value: int) -> None:
+        self.l[key] = value
+
+    def get(self, key: int) -> int:
+        return self.l[key]
+
+    def remove(self, key: int) -> None:
+        self.l[key] = -1
