58fc558f9dace5049532ac4b65c10364cc77be9e
Sergey <sproshchenko@yandex.ru>
2022-04-29 15:02:08 +0300

    - problem solved problem_0785_is_graph_bipartite.py

---
+++ problems/algorithms/problem_0785_is_graph_bipartite.py
@@ -0,0 +1,24 @@
+from typing import List
+from collections import deque
+
+
+class Solution:
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        N = len(graph)
+        col = [-1] * N
+
+        for i in range(N):
+            if col[i] != -1:
+                continue
+            q = deque()
+            q.append((i, 0))
+
+            while q:
+                node, color = q.popleft()
+                if col[node] == -1:
+                    col[node] = color
+                    for nx in graph[node]:
+                        q.append((nx, color ^ 1))
+                if col[node] != color:
+                    return False
+        return True
