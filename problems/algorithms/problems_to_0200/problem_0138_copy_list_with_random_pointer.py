85b1db309deeea40b6f07e43f4c9998feb8792ab
Sergey <sproshchenko@yandex.ru>
2023-07-08 15:45:14 +0300

    - problems were solved 0138, 0707

---
+++ problems/algorithms/problems_to_0200/problem_0138_copy_list_with_random_pointer.py
@@ -0,0 +1,39 @@
+from typing import Optional
+
+
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+
+        node = head
+        while node:
+            next = node.next
+            copy = Node(node.val, node.next, node.random)
+            node.next = copy
+            copy.next = next
+
+            node = next
+
+        node = head
+        while node:
+            if node.random:
+                node.next.random = node.random.next
+            node = node.next.next
+
+        dummy = Node(0)
+        prev = dummy
+        node = head
+
+        while node:
+            prev.next = node.next
+            node.next = node.next.next
+            node = node.next
+            prev = prev.next
+
+        return dummy.next
