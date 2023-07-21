60ddd2bcd64eebfcc7e0344c9e9864c11e216a26
Sergey <sproshchenko@yandex.ru>
2023-07-21 21:00:38 +0300

    - problems were solved 0122, 0744

---
+++ problems/algorithms/problem_0744_find_smallest_letter_greater_than_target.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        if target < letters[0] or target >= letters[-1]:
+            return letters[0]
+
+        l, r = 0, len(letters) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if target >= letters[mid]:
+                l = mid + 1
+            if target < letters[mid]:
+                r = mid - 1
+        return letters[l]
