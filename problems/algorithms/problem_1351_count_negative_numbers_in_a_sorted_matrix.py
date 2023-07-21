1c36778d89b884a78bf9eeb82fc6984ac8ccfd25
Sergey <sproshchenko@yandex.ru>
2023-07-21 23:51:32 +0300

    - problems were solved 1351

---
+++ problems/algorithms/problem_1351_count_negative_numbers_in_a_sorted_matrix.py
@@ -0,0 +1,38 @@
+from typing import List
+
+
+# T: O(m * log(n)
+class Solution:
+    def countNegatives(self, grid: List[List[int]]) -> int:
+        count = 0
+
+        for row in grid:
+            l, r = 0, len(row) - 1
+
+            while l <= r:
+                m = (l + r) // 2
+                if row[m] < 0:
+                    r = m - 1
+                else:
+                    l = m + 1
+
+            count += (len(row) - l)
+        return count
+
+
+# T: O(n+m)
+class Solution2:
+    def countNegatives(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        count = 0
+
+        row = 0
+        col = cols - 1
+
+        while row < rows and col >= 0:
+            if grid[row][col] < 0:
+                count += rows - row
+                col -= 1
+            else:
+                row += 1
+        return count
