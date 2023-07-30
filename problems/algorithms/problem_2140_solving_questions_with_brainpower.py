6fca5205bf79f825e33dcf85827978233c167a2b
Sergey <sproshchenko@yandex.ru>
2023-07-30 11:27:39 +0300

    - problems were solved 2140, 2466

---
+++ problems/algorithms/problem_2140_solving_questions_with_brainpower.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def mostPoints(self, questions: List[List[int]]) -> int:
+        dp = {}
+
+        for i in range(len(questions) - 1, -1, -1):
+            dp[i] = max(
+                questions[i][0] + dp.get(i + 1 + questions[i][1], 0),
+                dp.get(i + 1, 0)
+            )
+        return dp[0]
