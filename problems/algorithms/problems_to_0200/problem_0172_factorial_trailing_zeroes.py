af547de3cc39b5140681cc544237bc72cbfd989e
Sergey <sproshchenko@yandex.ru>
2023-10-29 16:19:40 +0300

    - problems were solved 0172

---
+++ problems/algorithms/problems_to_0200/problem_0172_factorial_trailing_zeroes.py
@@ -0,0 +1,10 @@
+
+
+class Solution:
+    def trailingZeroes(self, n: int) -> int:
+        oc = 5
+        count = 0
+        while n / oc >= 1:
+            count += n // oc
+            oc = oc * 5
+        return count
