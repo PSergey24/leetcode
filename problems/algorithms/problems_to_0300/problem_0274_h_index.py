8d602491c90c2a127bd25080aac61a43c49f9413
Sergey <sproshchenko@yandex.ru>
2023-11-05 18:01:57 +0300

    - problems were solved 0274

---
+++ problems/algorithms/problems_to_0300/problem_0274_h_index.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+# T: O(n*log(n))
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        n = len(citations)
+        citations.sort()
+
+        for i, v in enumerate(citations):
+            if n - i <= v:
+                return n - i
+        return 0
+
