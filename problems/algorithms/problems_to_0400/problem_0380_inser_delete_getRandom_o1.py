ba53b927505326b684ec60fc02ab537782ef91a3
Sergey <sproshchenko@yandex.ru>
2023-11-05 22:16:13 +0300

    - problems were solved 0380

---
+++ problems/algorithms/problems_to_0400/problem_0380_inser_delete_getRandom_o1.py
@@ -0,0 +1,29 @@
+import random
+
+
+class RandomizedSet:
+
+    def __init__(self):
+        self.numMap = {}
+        self.numList = []
+
+    def insert(self, val: int) -> bool:
+        res = val not in self.numMap
+        if res:
+            self.numMap[val] = len(self.numList)
+            self.numList.append(val)
+        return res
+
+    def remove(self, val: int) -> bool:
+        res = val in self.numMap
+        if res:
+            index = self.numMap[val]
+            lastVal = self.numList[-1]
+            self.numList[index] = lastVal
+            self.numList.pop()
+            self.numMap[lastVal] = index
+            del self.numMap[val]
+        return res
+
+    def getRandom(self) -> int:
+        return random.choice(self.numList)
